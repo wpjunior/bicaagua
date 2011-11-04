94c827e0b35bd39c936fe00c0b96d49d5cb094ee
Wilson Júnior <wilsonpjunior@gmail.com>
2011-11-04 13:09:13 -0200

    alterado o design do site, adicionado plugin de troca de cabeçalho

--- wikipages/static/js/tiny_mce/textareas.js
+++ wikipages/static/js/tiny_mce/textareas.js
@@ -5,7 +5,7 @@ tinyMCE.init({
     //content_css : "/appmedia/blog/style.css",
     theme_advanced_toolbar_location : "top",
     theme_advanced_toolbar_align : "left",
-    theme_advanced_buttons1 : "fullscreen,separator,preview,separator,bold,italic,underline,strikethrough,separator,bullist,numlist,outdent,indent,separator,undo,redo,separator,link,unlink,anchor,separator,image,cleanup,help,separator,code,table",
+    theme_advanced_buttons1 : "fullscreen,separator,preview,separator,bold,italic,underline,strikethrough,separator,bullist,numlist,outdent,indent,separator,undo,redo,separator,link,unlink,separator,image,cleanup,separator,code,table",
     theme_advanced_buttons2 : "",
     theme_advanced_buttons3 : "",
     auto_cleanup_word : true,
